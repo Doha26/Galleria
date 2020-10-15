24fe161e50d4500ceb234d5e887630012686884d
Pavel <foujeupavel@gmail.com>
2020-10-16 00:27:34 +0100

    Fixing bad import

--- src/screens/Intro/index.tsx
+++ src/screens/Intro/index.tsx
@@ -4,8 +4,8 @@ import React from 'react';
 import {NavigationScreenProp} from 'react-navigation';
 import styles from '~/screens/Intro/styles';
 import BackgroundImage from '~/components/config/BackgroundImage';
-import Index from '~/components/common/Container';
-import Index from '~/components/common/Button';
+import Container from '~/components/common/Container';
+import Button from '~/components/common/Button';
 import colors from '~/theming/colors';
 
 const slideBg = require('~/assets/images/bg-intro.jpg');
@@ -16,7 +16,7 @@ const getStarted = (navigate: Function) => {
 
 const IntroScreen = ({navigation: {navigate}}: {navigation: NavigationScreenProp<any>}) => {
   return (
-    <Index>
+    <Container>
       <BackgroundImage source={slideBg} style={styles.backgroundImage}>
         <ScrollView contentContainerStyle={{flex: 1}}>
           <SafeAreaView style={styles.rootView}>
@@ -26,7 +26,7 @@ const IntroScreen = ({navigation: {navigate}}: {navigation: NavigationScreenProp
               <Text style={styles.subheading}>
                 Let&apos;s browse some awesome images from around the worlds`
               </Text>
-              <Index
+              <Button
                 text="Get started"
                 color={colors.pink}
                 onPress={() => {
@@ -38,7 +38,7 @@ const IntroScreen = ({navigation: {navigate}}: {navigation: NavigationScreenProp
           </SafeAreaView>
         </ScrollView>
       </BackgroundImage>
-    </Index>
+    </Container>
   );
 };
 export default IntroScreen;
